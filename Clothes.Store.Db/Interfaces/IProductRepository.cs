d60686aa9f670936869e53cdcd0a0190b23b997f
Αναστάσιος Παλασόπουλος <a_palasopoulos@yahoo.com>
2024-04-07 21:11:44 +0300

    Wip==>Created Stored Procedure for Inserting Products.

--- Clothes.Store.Db/Interfaces/IProductRepository.cs
+++ Clothes.Store.Db/Interfaces/IProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Clothes.Store.Common.Models.Result;
+using Clothes.Store.Common.Responses;
 using Clothes.Store.Db.DbEntities;
 
 namespace Clothes.Store.Db.Interfaces
@@ -6,5 +7,6 @@ namespace Clothes.Store.Db.Interfaces
     public interface IProductRepository
     {
         public Task<Result<IEnumerable<Product>>> GetProductsFromDb();
+        public Task<Result<bool>> InsertProductsAsync(IEnumerable<ProductResponse> productResponses);
     }
 }
--- Clothes.Store.Db/Repository/ProductRepository.cs
+++ Clothes.Store.Db/Repository/ProductRepository.cs
@@ -1,8 +1,11 @@
 ﻿using Clothes.Store.Common.Models.Result;
+using Clothes.Store.Common.Responses;
 using Clothes.Store.Db.Context;
 using Clothes.Store.Db.DbEntities;
+using Clothes.Store.Db.Extensions;
 using Clothes.Store.Db.Interfaces;
 using Dapper;
+using System.Data;
 
 namespace Clothes.Store.Db.Repository
 {
@@ -26,6 +29,40 @@ namespace Clothes.Store.Db.Repository
             catch (Exception ex)
             {
                 return Result<IEnumerable<Product>>.Exception(Clothes.Store.Common.Models.Result.ResponseCodes.Codes.InternalError, ex);
+            }
+        }
+
+
+        public async Task<Result<bool>> InsertProductsAsync(IEnumerable<ProductResponse> productResponses)
+        {
+            var connection = _context.CreateConnection();
+            IDbTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                foreach (var response in productResponses)
+                {
+                    var product = response.ToProduct(); 
+
+                    var parameters = new DynamicParameters();
+                    parameters.Add("@Id", product.Id, DbType.Int32);
+                    parameters.Add("@Title", product.Title, DbType.String);
+                    parameters.Add("@Price", product.Price, DbType.Decimal);
+                    parameters.Add("@Description", product.Description, DbType.String);
+                    parameters.Add("@Category", product.Category, DbType.String);
+                    parameters.Add("@Image", product.Image, DbType.String);
+                    parameters.Add("@Rating", product.Rating, DbType.String); 
+
+                    await connection.ExecuteAsync("InsertProductResponseFromWebService", parameters, transaction, commandType: CommandType.StoredProcedure);
+                }
+
+                transaction.Commit();
+                return Result<bool>.ActionSuccessful(true, Clothes.Store.Common.Models.Result.ResponseCodes.Codes.OK);
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                return Result<bool>.Exception(Clothes.Store.Common.Models.Result.ResponseCodes.Codes.InternalError, ex);
             }
         }
     }
