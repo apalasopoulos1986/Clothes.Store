a5394cb1f0738cb0fadd459599d4fc4992975e05
Αναστάσιος Παλασόπουλος <a_palasopoulos@yahoo.com>
2024-04-08 23:09:55 +0300

    Minor Corrections

--- Clothes.Store.Service/Extensions/JsonExtensions.cs
+++ Clothes.Store.Service/Extensions/JsonExtensions.cs
@@ -19,21 +19,6 @@ namespace Clothes.Store.Service.Extensions
 
         }
        
-        public static Product ToProduct(this ProductResponse response)
-        {
-           
-            var product = new Product
-            {
-                Id = response.Id,
-                Title = response.Title,
-                Price = response.Price,
-                Description = response.Description,
-                Category = response.Category,
-                Image = response.Image,
-                Rating = JsonConvert.SerializeObject(response.Rating)
-            };
-
-            return product;
-        }
+       
     }
 }
