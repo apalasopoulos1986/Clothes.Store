65d88bbb085816f85a59f48f8e6479589f0742a4
Αναστάσιος Παλασόπουλος <a_palasopoulos@yahoo.com>
2024-04-09 11:30:25 +0300

    Removed the foreign key constraints and add IsUserActive Column in Purchases Table

--- Clothes.Store.Db/Sql.Scripts/Clothes_Store_Schema_Creation.sql
+++ Clothes.Store.Db/Sql.Scripts/Clothes_Store_Schema_Creation.sql
@@ -60,24 +60,25 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+USE [ClothesStore]
+GO
+
+/****** Object:  Table [dbo].[Purchases]    Script Date: 9/4/2024 11:21:16 πμ ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 CREATE TABLE [dbo].[Purchases](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[UserId] [int] NULL,
 	[ProductId] [int] NULL,
 	[PurchaseDate] [datetime] NULL,
+	[IsUserActive] [bit] NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-ALTER TABLE [dbo].[Purchases]  WITH CHECK ADD FOREIGN KEY([ProductId])
-REFERENCES [dbo].[Products] ([Id])
-GO
-
-ALTER TABLE [dbo].[Purchases]  WITH CHECK ADD FOREIGN KEY([UserId])
-REFERENCES [dbo].[Users] ([Id])
-GO
-
-
