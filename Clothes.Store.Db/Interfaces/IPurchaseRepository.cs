0c697285eb78913b62de360e12e5d7c1b9662ce9
Αναστάσιος Παλασόπουλος <a_palasopoulos@yahoo.com>
2024-04-09 20:47:06 +0300

    WIP

--- Clothes.Store.Db/Interfaces/IPurchaseRepository.cs
+++ Clothes.Store.Db/Interfaces/IPurchaseRepository.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Clothes.Store.Common.Models.Result;
+using Clothes.Store.Db.DbEntities;
+
 
 namespace Clothes.Store.Db.Interfaces
 {
     public interface IPurchaseRepository
     {
+        public  Task<Result<bool>> PurchaseProductAsync(int userId, int productId);
+        public  Task<Result<List<User>>> GetUsersByProductIdAsync(int productId);
+
+        public Task<Result<List<Product>>> GetPurchasedProductsByUserIdAsync(int userId);
     }
 }
